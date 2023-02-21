eab51985ade23749509fcd8cb9d4f72df2fc3690
Collin Smallegan <cmsmallegan@comsma.com>
2023-02-21 12:56:57 -0500

    Updated docker file

--- server/main.go
+++ server/main.go
@@ -23,7 +23,7 @@ var client *typesense.Client
 func main() {
 
 	client = typesense.NewClient(
-		typesense.WithServer("localhost:8108"),
+		typesense.WithServer("http://localhost:8108"),
 		typesense.WithAPIKey(os.Getenv("TS_API_KEY")),
 	)
 	schemas.RegisterSchemas(client)
